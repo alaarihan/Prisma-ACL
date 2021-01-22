feacce5d59d5990a53b72ebe4da8fd4cfaaf1cc9
Adam Sultan <adamsultan7@gmail.com>
2021-01-22 09:48:33 +0200

    Add a middleware for usewr mutations to auto hash password

---
+++ src/middlewares/userMutationsMiddleware.ts
@@ -0,0 +1,31 @@
+import { hash, compare } from "bcrypt";
+
+export const userMutationsMiddleware = async (
+  { args, context, info },
+  next
+) => {
+  const types = ["Query", "Mutation"];
+  const byPassList = ["deleteOneUser", "deleteManyUser"];
+  if (
+    types.includes(info.path.typename) &&
+    !byPassList.includes(info.fieldName)
+  ) {
+    if (
+      info.fieldName === "createOneUser" ||
+      info.fieldName === "updateOneUser" ||
+      info.fieldName === "updateManyUser"
+    ) {
+      if (args.data.password) {
+        args.data.password = await hash(args.data.password, 10);
+      }
+    } else if (info.fieldName === "upsertOneUser") {
+      if (args.update && args.update.password) {
+        args.update.password = await hash(args.update.password, 10);
+      } 
+      if(args.create && args.create.password) {
+        args.create.password = await hash(args.create.password, 10);
+      }
+    }
+  }
+  return next();
+};
--- src/modules/User/User.module.ts
+++ src/modules/User/User.module.ts
@@ -1,9 +1,15 @@
 import { createModule } from 'graphql-modules'
 import typeDefs from './typeDefs'
 import resolvers from './resolvers'
+import { userMutationsMiddleware } from "../../middlewares/userMutationsMiddleware";
 
 export const UserModule = createModule({
   id: 'User',
   typeDefs,
   resolvers,
+  middlewares: {
+    "Mutation": {
+      "*": [userMutationsMiddleware]
+    }
+  }
 })
