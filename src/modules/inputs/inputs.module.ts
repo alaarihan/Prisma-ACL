a77c8ddcf9cd6cae89b4c3fd76ffdd2944887ec9
Adam Sultan <adamsultan7@gmail.com>
2021-01-22 09:47:37 +0200

    Exclude some input fields from being generated

--- src/modules/inputs/inputs.module.ts
+++ src/modules/inputs/inputs.module.ts
@@ -1,7 +1,33 @@
 import { createModule, gql } from "graphql-modules";
-import { sdlInputs } from "@paljs/plugins";
+import { sdlInputs } from "../../../tools/sdlInputsGen";
 
+const userSecretFields = ["password", "verificationToken"];
+const excludedFields = [
+  {
+    name: "UserWhereInput",
+    fields: userSecretFields,
+  },
+  {
+    name: "UserOrderByInput",
+    fields: userSecretFields,
+  },
+  {
+    name: "UserCreateInput",
+    fields: ['verificationToken'],
+  },
+  {
+    name: "UserUpdateInput",
+    fields: ['verificationToken'],
+  },
+  {
+    name: "UserUpdateManyMutationInput",
+    fields: ['verificationToken'],
+  },
+];
 export const InputsModule = createModule({
   id: "Inputs",
-  typeDefs: sdlInputs({doNotUseFieldUpdateOperationsInput: true}),
+  typeDefs: sdlInputs({
+    doNotUseFieldUpdateOperationsInput: true,
+    exclude: excludedFields,
+  }),
 });
