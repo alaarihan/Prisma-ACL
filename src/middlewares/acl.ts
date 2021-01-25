eed2d847f5c28cf1364c40cc45754cc4cc3eebdd
Adam Sultan <adamsultan7@gmail.com>
2021-01-25 19:53:14 +0200

    Add acl for update and delete mutations

--- src/middlewares/acl.ts
+++ src/middlewares/acl.ts
@@ -39,6 +39,7 @@ export const acl = async ({ parent, args, context, info }, next) => {
         throw new Error(err.message);
       });
     if (!permissions || !permissions.id) throw new Error("ACL problem!");
+    // Read types
     if (readTypes.includes(info.fieldName)) {
       const hasReadAccess = checkUserPermission("read", permissions);
       if (!hasReadAccess || hasReadAccess === "NONE") {
@@ -79,6 +80,104 @@ export const acl = async ({ parent, args, context, info }, next) => {
           return null;
         }
       } else if (hasReadAccess === "ALL") {
+        return next();
+      }
+    }
+    // Update types
+    if (updateMany === info.fieldName) {
+      const hasUpdateAccess = checkUserPermission("update", permissions);
+      if (!hasUpdateAccess || hasUpdateAccess === "NONE") {
+        throw new ApolloError("Forbidden!", "Forbidden");
+      } else if (hasUpdateAccess === "OWN") {
+        if (!args.where) {
+          args.where = {};
+        }
+        if (!args.where.AND) {
+          args.where.AND = [];
+        }
+        if (!args.where.AND[1]) {
+          args.where.AND[1] = {};
+        }
+        if (!args.where.AND[1].authorId) {
+          args.where.AND[1].authorId = {};
+        }
+        args.where.AND[1].authorId.equals = context.user.id;
+        return next();
+      } else if (hasUpdateAccess === "ALL") {
+        return next();
+      }
+    }
+    if (updateOne === info.fieldName) {
+      const hasUpdateAccess = checkUserPermission("update", permissions);
+      if (!hasUpdateAccess || hasUpdateAccess === "NONE") {
+        throw new ApolloError("Forbidden!", "Forbidden");
+      } else if (hasUpdateAccess === "OWN") {
+        if (context.moduleId !== "User") {
+          args.select.authorId = true;
+        }
+        const item = await prisma[context.moduleId.toLowerCase()].findUnique({
+          where: args.where,
+          rejectOnNotFound: true
+        }).catch(err => { throw new Error(err)});
+        if (
+          (item && item.authorId === context.user.id) ||
+          (context.moduleId === "User" && item && item.id === context.user.id)
+        ) {
+          return next();
+        } else {
+          throw new ApolloError("Forbidden!", "Forbidden");
+        }
+      } else if (hasUpdateAccess === "ALL") {
+        return next();
+      }
+    }
+
+    // Delete types
+    if (deleteMany === info.fieldName) {
+      const hasDeleteAccess = checkUserPermission("delete", permissions);
+      if (!hasDeleteAccess || hasDeleteAccess === "NONE") {
+        throw new ApolloError("Forbidden!", "Forbidden");
+      } else if (hasDeleteAccess === "OWN") {
+        if (context.moduleId === "User")
+          throw new ApolloError("Forbidden!", "Forbidden");
+        if (!args.where) {
+          args.where = {};
+        }
+        if (!args.where.AND) {
+          args.where.AND = [];
+        }
+        if (!args.where.AND[1]) {
+          args.where.AND[1] = {};
+        }
+        if (!args.where.AND[1].authorId) {
+          args.where.AND[1].authorId = {};
+        }
+        args.where.AND[1].authorId.equals = context.user.id;
+        return next();
+      } else if (hasDeleteAccess === "ALL") {
+        return next();
+      }
+    }
+    if (deleteOne === info.fieldName) {
+      const hasDeleteAccess = checkUserPermission("delete", permissions);
+      if (!hasDeleteAccess || hasDeleteAccess === "NONE") {
+        throw new ApolloError("Forbidden!", "Forbidden");
+      } else if (hasDeleteAccess === "OWN") {
+        if (context.moduleId === "User")
+          throw new ApolloError("Forbidden!", "Forbidden");
+        const item = await prisma[context.moduleId.toLowerCase()].findUnique({
+          where: args.where,
+          rejectOnNotFound: true
+        }).catch(err => { throw new Error(err)});
+        if (
+          (item && item.authorId === context.user.id) ||
+          (context.moduleId === "User" && item && item.id === context.user.id)
+        ) {
+          return next();
+        } else {
+          throw new ApolloError("Forbidden!", "Forbidden");
+        }
+      } else if (hasDeleteAccess === "ALL") {
         return next();
       }
     }
