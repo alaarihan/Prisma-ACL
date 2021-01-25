259926e29ca7bb9c3125f7ad4111a70a816cbc9f
Adam Sultan <adamsultan7@gmail.com>
2021-01-25 19:14:15 +0200

    Auto fill authorId for log in logger middleware

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -3,15 +3,20 @@ import { prisma } from "../common/prisma";
 export const logger = async ({ parent, context, info }, next) => {
   const logTypes = ["Query", "Mutation"];
   if (logTypes.includes(info.path.typename)) {
+    const log = {
+      operation: info.fieldName,
+      ip: context.req.connection.remoteAddress.replace("::ffff:", ""),
+      host: context.req.headers.host,
+      userAgent: context.req.headers["user-agent"],
+      referer: context.req.headers.referer,
+      authorId: null,
+    };
+    if (context.user && context.user.id) {
+      log.authorId = context.user.id;
+    }
     await prisma.log
       .create({
-        data: {
-          operation: info.fieldName,
-          ip: context.req.connection.remoteAddress.replace("::ffff:", ""),
-          host: context.req.headers.host,
-          userAgent: context.req.headers["user-agent"],
-          referer: context.req.headers.referer,
-        },
+        data: log,
       })
       .catch((err) => console.log(err));
   }
